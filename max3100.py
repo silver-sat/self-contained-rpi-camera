77b46a7249e7984f736d5dffc63d80830d6d0bbe
Nathan Edwards <edwardsnj@gmail.com>
2022-05-01 13:18:35 -0400

    class-based camera, button, and max3100 code

---
+++ max3100.py
@@ -0,0 +1,208 @@
+#!/bin/env python3
+
+import spidev
+import serial
+import sys
+import time
+import array
+import itertools
+import hashlib
+import random
+
+class MAX3100(object):
+
+    # Commands.
+    CMD_WRITE_CONF      = 0b1100000000000000
+    CMD_READ_CONF       = 0b0100000000000000
+    CMD_WRITE_DATA      = 0b1000000000000000
+    CMD_READ_DATA       = 0b0000000000000000
+    
+    # Configuration.
+    CONF_R              = 0b1000000000000000
+    CONF_T              = 0b0100000000000000
+    CONF_RM             = 0b0000010000000000
+
+    # Baud rates for clock multiplier x1.
+    CONF_BAUD_X1_115200 = 0b0000000000000000
+    CONF_BAUD_X1_57600  = 0b0000000000000001
+    CONF_BAUD_X1_38400  = 0b0000000000001000
+    CONF_BAUD_X1_19200  = 0b0000000000001001
+    CONF_BAUD_X1_9600   = 0b0000000000001010
+    CONF_BAUD_X1_4800   = 0b0000000000001011
+    CONF_BAUD_X1_2400   = 0b0000000000001100
+    CONF_BAUD_X1_1200   = 0b0000000000001101
+    CONF_BAUD_X1_600    = 0b0000000000001110
+    CONF_BAUD_X1_300    = 0b0000000000001111
+    
+    # Baud rates for clock multiplier x2.
+    CONF_BAUD_X2_230400 = 0b0000000000000000
+    CONF_BAUD_X2_115200 = 0b0000000000000001
+    CONF_BAUD_X2_57600  = 0b0000000000000010
+    CONF_BAUD_X2_38400  = 0b0000000000001001
+    CONF_BAUD_X2_19200  = 0b0000000000001010
+    CONF_BAUD_X2_9600   = 0b0000000000001011
+    CONF_BAUD_X2_4800   = 0b0000000000001100
+    CONF_BAUD_X2_2400   = 0b0000000000001101
+    CONF_BAUD_X2_1200   = 0b0000000000001110
+    CONF_BAUD_X2_600    = 0b0000000000001111
+
+    CRYSTAL_1843_kHz = 1
+    CRYSTAL_3686_kHz = 2
+
+    def __init__(self,spiif=0,spiselect=0,spispeed=7800000,
+                      crystal_kHz=3686,baud=9600):
+        assert hasattr(self,f"CRYSTAL_{crystal_kHz}_kHz"), f"No MAX3100.CRYSTAL_{crystal_kHz}_kHz constant"
+        crystal = getattr(self,f"CRYSTAL_{crystal_kHz}_kHz")
+        assert hasattr(self,f"CONF_BAUD_X{crystal}_{baud}"), f"No MAX3100.CONF_BAUD_X{crystal}_{baud} constant"
+                
+        self.spi = spidev.SpiDev()
+        self.spi.open(spiif,spiselect)
+        self.spi.max_speed_hz = spispeed
+        self.spi.mode = 0b00
+
+        conf = getattr(self,f"CONF_BAUD_X{crystal}_{baud}")
+        word = ( self.CMD_WRITE_CONF | self.CONF_RM | conf )
+        self.xferword(word)
+
+    @staticmethod
+    def tobytearray(words):
+        msws = map(lambda w: (w&0xffff)>>8, words)
+        lsws = map(lambda w: (w&0xff), words)
+        return array.array('B',[ item for pair in zip(msws,lsws) for item in pair ])
+
+    @staticmethod
+    def frombytearray(bytes):
+        return [ (bytes[i]<<8 | bytes[i+1]) for i in range(0,len(bytes),2) ]
+        
+    def tobytelist(self,word):
+        return [ (word&0xffff)>>8, word&0xff ]
+
+    def frombytelist(self,bytelist):
+        return (bytelist[0]<<8 | bytelist[1])
+
+    def xferword(self,word,msg=None):
+        if msg:
+            self.print_word(msg+":send",word)
+        rword = self.frombytelist(self.spi.xfer(self.tobytelist(word)))
+        if msg:
+            self.print_word(msg+":read",rword)
+        return rword
+
+    readcmd = tobytearray.__func__([CMD_READ_DATA])
+    def readbytes(self):
+        byteslist = []
+        while True:
+            bytesi = list(filter(lambda t: t[0]&0x80,( self.spi.xfer(self.readcmd) for _ in range(128))))
+            if len(bytesi) == 0:
+                break           
+            byteslist.extend(bytesi)
+        return bytes(t[1] for t in byteslist)
+
+    def read(self):
+        r = self.xferword(self.CMD_READ_DATA)
+        if r&self.CONF_R:
+            return r&0xff
+        return None
+
+    def available(self):
+        r = self.xferword(self.CMD_READ_CONF)
+        return bool(r&self.CONF_R)
+
+    def write(self,byte):
+        while True:
+            if not self._busy():
+                r = self.xferword((self.CMD_WRITE_DATA | (byte & 0xff)))
+                break
+        return True
+
+    def _busy(self):
+        r = self.xferword(self.CMD_READ_CONF)
+        return (not (r & self.CONF_T))
+
+    readconf = tobytearray.__func__([CMD_READ_CONF])
+    writecmd = tobytearray.__func__([CMD_WRITE_DATA])
+    def writebytes(self,buffer):
+        for b in buffer: 
+            self.writecmd[1] = b
+            while True:
+                # (return_value & CONF_T)
+                if self.spi.xfer(self.readconf)[0]&0x40:
+                    break
+            self.spi.xfer(self.writecmd)
+
+    def print_word(self,prefix,word,file=sys.stderr):
+        binstr = bin(word)[2:]
+        binstr = (16-len(binstr))*'0' + binstr
+        binstr = " ".join([binstr[:8],binstr[8:]])
+        chr1 = chr(word>>8) if (33 <= (word>>8) <= 126) else "?"
+        chr2 = chr(word&0xff) if (33 <= (word&0xff) <= 126) else "?"
+        hexstr = hex(word)[2:].upper()
+        hexstr = (4-len(hexstr))*'0' + hexstr
+        print('%s: bin:%s hex:%s chr:%s%s'%(prefix,binstr,hexstr,chr1,chr2),file=file)
+
+
+if __name__ == "__main__":
+
+    # MAX3100 Loop-back via Serial Port test...
+
+    from multiprocessing import Process
+
+    def max3100_thread(baud,length):
+        max3100 = MAX3100(baud=baud)
+        while True:
+            start = time.time()
+            bytes = max3100.readbytes()
+            if len(bytes) > 0:
+                break
+        print("%4d characters (%s) received (SPI) in %f seconds"%(len(bytes),hashlib.md5(bytes).hexdigest().lower(),time.time()-start))
+
+        time.sleep(2)
+
+        print("%4d characters (%s) to send (SPI)"%(len(bytes),hashlib.md5(bytes).hexdigest().lower()))
+        max3100.writebytes(bytes)
+
+        time.sleep(2)
+
+        while True:
+            bytes = "".join(chr(ord('A')+random.randint(0,25)) for i in range(0,6)).encode('utf8')
+            print("%4d characters (%s) to send (SPI)"%(len(bytes),bytes))
+            max3100.writebytes(bytes)
+            bytes = max3100.readbytes()
+            print("%4d characters (%s) received (SPI)"%(len(bytes),bytes))
+            time.sleep(1)
+
+
+    def serial_thread(baud,length):
+        time.sleep(1)
+        ser = serial.Serial('/dev/serial0',baud)
+        ser.reset_output_buffer()
+        ser.reset_input_buffer()
+        str = "".join(chr(ord('A')+random.randint(0,25)) for i in range(0,length))
+        print("%4d characters (%s) to send (serial,%d baud)"%(len(str),hashlib.md5(str.encode('utf8')).hexdigest().lower(),baud))
+        ser.write(str.encode('utf8'))
+        time.sleep(0.1)
+        while ser.in_waiting == 0:
+            pass
+        start = time.time()
+        str = ser.read(length).decode('utf8')
+        print("%4d characters (%s) received (serial) in %f seconds"%(len(str),hashlib.md5(str.encode('utf8')).hexdigest().lower(),time.time()-start))
+        time.sleep(1)
+        while True:
+            bytes = ser.read(6)
+            ser.write(bytes)
+
+        ser.close()
+        time.sleep(1)
+
+    serial_baud = 38400
+    length = 1024
+
+    p1 = Process(target=serial_thread,args=(serial_baud,length))
+    p1.start()
+
+    p2 = Process(target=max3100_thread,args=(serial_baud,length))
+    p2.start()
+
+    p1.join()
+    p2.join()
+
---
+++ standalone_camera.py
@@ -0,0 +1,253 @@
+
+# Based on code by Dave Copeland and Xander Hostler
+
+import serial
+import time, os, os.path
+import RPi.GPIO as GPIO
+
+class uCAM_III(object):
+
+    # BCM number for reset pin on GPIO header
+    default_resetPin = 23
+    # time to wait before reading camera response
+    default_responseDelay = 1
+    # time to wait between commands
+    default_interCommandDelay = 1
+
+    # uCAM III protocol...
+    INITIAL = (0xAA,0x01,0x00,0x00,0x00,0x00)
+    # INITIAL_JPEG = (0xAA,0x01,0x00,0x07,0x07,0x03)  # 160x128 format      
+    # INITIAL_JPEG = (0xAA,0x01,0x00,0x07,0x07,0x05)  # 320x240 format      
+    INITIAL_JPEG = (0xAA,0x01,0x00,0x07,0x07,0x07)  # 640x480 format      
+    SET_PKG_SIZE = (0xAA,0x06,0x08,0x00,0x02,0x00)
+    SNAPSHOT = (0xAA,0x05,0x00,0x00,0x00,0x00)
+    GETPICTURE = (0xAA,0x04,0x01,0x00,0x00,0x00)
+    FULL_RESET = (0xAA,0x08,0x00,0x00,0x00,0x00)
+    SYNC = (0xAA,0x0D,0x00,0x00,0x00,0x00)
+    ACK = (0xAA,0x0E)
+    LIGHT = (0xAA,0x13,0x01,0x00,0x00,0x00)
+    EXPSETTINGS = (0xAA,0x14,0x02,0x02,0x02,0x00)
+
+    pkgSize = 512
+
+    def __init__(self,**kw):
+        self.resetPin = kw.get('resetPin',self.default_resetPin)
+        self.responseDelay = kw.get('responseDelay',self.default_responseDelay)
+        self.interCommandDelay = kw.get('interCommandDelay',self.default_interCommandDelay)
+        self.verbose = kw.get('verbose',False)
+        GPIO.setup(self.resetPin,GPIO.OUT)
+
+    def reset(self):
+        GPIO.output(self.resetPin,GPIO.LOW)
+        time.sleep(0.1)
+        GPIO.output(self.resetPin,GPIO.HIGH)
+
+    def full_reset(self):
+        self.command(self.FULL_RESET)
+
+    def command(self,bytes,**kw):
+        responseDelay = kw.get('responseDelay',self.responseDelay)
+        verbose=kw.get('verbose',self.verbose)
+        wait=kw.get('wait',True)
+        if verbose:
+            print("Write:",len(bytes),":"," ".join(map(lambda b: "%02X"%b,bytes)))
+        self.write(bytes)
+        time.sleep(responseDelay)
+        retval = self.read()
+        while wait and len(retval) == 0:
+            retval = self.read()
+        if verbose:
+            print("Read: ",len(retval),":"," ".join(map(lambda b: "%02X"%b,retval)))
+        return retval
+
+    def sync(self):
+        maxTries = 60
+        currentTry = 0
+        reply = b''
+        delayValue = 0.005
+        delayIncrement = 0.001
+    
+        while (currentTry < maxTries) and (len(reply) != 12 or reply[:3] != b'\xaa\x0e\x0d'):
+            # print("Sync attempt:",currentTry)
+            delayValue += delayIncrement
+            reply = self.command(self.SYNC, responseDelay=0.05, wait=False)
+            currentTry +=1
+
+        if len(reply) != 12 or reply[:3] != b'\xaa\x0e\x0d':
+            # Didn't work
+            print(reply[:3])
+            print("Sync didn't work. Sent %d tries" % currentTry)
+            return False
+
+        # it did work
+        print("Camera replied", reply, "attempts:",currentTry)
+        # send the ACK back to camera        
+        bytes = self.ACK+(0x0D,0x00,0x00,0x00)
+        if self.verbose:
+            print("Write:",len(bytes),":"," ".join(map(lambda b: "%02X"%b,bytes)))
+        response = self.write(self.ACK+(0x0D,0x00,0x00,0x00))
+        return True
+
+    def takephoto(self):
+        self.command(self.INITIAL_JPEG)
+        time.sleep(self.interCommandDelay)
+        self.command(self.SET_PKG_SIZE)
+        time.sleep(self.interCommandDelay)
+        self.command(self.EXPSETTINGS)
+        time.sleep(self.interCommandDelay)
+        self.command(self.SNAPSHOT)
+
+    def retrieveDataFromPkg(self,pkg):
+        # pkgID = pkg[0:2]
+        dataBuffer = pkg[4:-2]
+        return dataBuffer
+
+    def getPictureSize(self):
+        response = self.command(self.GETPICTURE)
+        print("Wrote GETPICTURE. Camera replied",response)
+        return response[-1]*256*256+response[-2]*256+response[-3]
+
+    def getPicture(self):
+        camReply = self.command(self.GETPICTURE)
+        # print("Wrote GETPICTURE. Camera replied",camReply)
+
+        replyLen=len(camReply)
+        imageSize = camReply[replyLen-1]*256*256+camReply[replyLen-2]*256+camReply[replyLen-3]
+        print("Picture size:",imageSize,"bytes")
+
+        numPackages = int(imageSize/(self.pkgSize-6))+1
+        # start the download by ACKing back
+        pkgCounter0 = 0
+        pkgCounter1 = 0  # I'm cheating and assuming num packages < 255!!
+        byteCounter = 0
+        pictureArray = b''
+
+        for indx in range(0,numPackages):
+            pkgCounter0 = indx
+            ackResponse = self.ACK+(0,0,pkgCounter0,pkgCounter1)
+            while True:
+                pkg = self.command(ackResponse)
+                pkgID0 = pkg[0]
+                pkgID1 = pkg[1]
+                print("Received package %d %d - size %d bytes" % (pkgID1,pkgID0,len(pkg)))
+                # the only tell for bad packet we have is if the package is short
+                if len(pkg) == min(self.pkgSize,imageSize-len(pictureArray)+6):
+                    break
+
+            pictureArray += self.retrieveDataFromPkg(pkg)
+            print("Received %6.2f%%" % (100.0*len(pictureArray)/imageSize,))
+
+        print("Received - %d  bytes" % len(pictureArray))    
+        return pictureArray
+
+    def savePicture(self,filename):
+        print("Picture filename:",filename)
+        start = time.time()
+        picture = self.getPicture()
+        print("Picture size:",len(picture))
+        picFile = open(filename,'wb')
+        picFile.write(picture)
+        picFile.close()
+        print("Time to save: %0f sec"%(time.time()-start,))
+
+class uCAM_III_Serial(uCAM_III):
+    # Constants
+    default_baudRate = 56000
+
+    # initialize
+    def __init__(self,serialDevice,**kw):
+        baudRate = kw.get('baudRate',self.default_baudRate)
+        self.serialPort = serial.Serial(serialDevice,baudRate)
+        super(uCAM_III_Serial,self).__init__(**kw)
+
+    def read(self):
+        readString = b''
+        bytesToRead = self.serialPort.in_waiting
+        while bytesToRead >0:
+            readByte= self.serialPort.read(1)
+            readString += readByte
+            bytesToRead = self.serialPort.in_waiting
+            time.sleep(0.00005)
+        return readString
+
+    def write(self,bytes):
+        self.serialPort.write(bytes)
+
+import max3100
+
+class uCAM_III_MAX3100(uCAM_III):
+    # Constants
+    # default_baudRate = 38400
+    # default_baudRate = 19200
+    default_baudRate = 9600
+
+    # initialize
+    def __init__(self,spiDevice,**kw):
+        baudRate = kw.get('baudRate',self.default_baudRate)
+        # other MAX3100 parameters are set to default values
+        self.max3100 = max3100.MAX3100(spiif=spiDevice[0],
+                                       spiselect=spiDevice[1],
+                                       baud=baudRate)
+        super(uCAM_III_MAX3100,self).__init__(**kw)
+
+    def read(self):
+        return self.max3100.readbytes()
+
+    def write(self,bytes):
+        self.max3100.writebytes(bytes)
+
+class Button(object):
+
+    default_buttonPin = 24
+
+    def __init__(self, **kw):
+        self.buttonPin = kw.get('buttonPin',self.default_buttonPin)
+        self.callback = kw.get('callback',self.callback)
+        GPIO.setup(self.buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(self.buttonPin,GPIO.RISING,callback=self.callback)
+
+    def callback(self,channel):
+        print("Button was pushed!")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+button_pressed = True
+
+def button_callback(channel):
+    global button_pressed
+    button_pressed = True
+    print("Button was pushed!")
+
+button = Button(buttonPin=24, callback=button_callback)
+
+camera = uCAM_III_Serial(serialDevice="/dev/serial0", 
+                         resetPin=23,
+                         responseDelay=0.2,
+                         verbose=True)
+
+# camera = uCAM_III_MAX3100(spiDevice=(0,0), 
+#                           resetPin=23,
+#                           responseDelay=0.2,
+#                           verbose=True)
+
+counter = 1
+while True:
+    if button_pressed:
+        print("Take photo")
+        camera.reset()
+        assert camera.sync()
+        time.sleep(2)
+        camera.takephoto()
+
+        print("Save photo")
+        filename = 'testPic-%03d.jpg'%(counter,)
+        camera.savePicture(filename)
+        counter += 1
+
+        button_pressed = False
+
+    time.sleep(.1)
+
+GPIO.cleanup()
+
